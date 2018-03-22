a50300998ed86a4b86adae280d169295fd4910de
Gyarmathy Timea <gyarmathy.timi@hotmail.com>
2018-03-22 20:19:36 +0200

    Changed back to MS SQL project

---
+++ SD1-pingpong/Dao/DbConnection.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Dao
+{
+    public class DbConnection
+    {
+        private SqlDataAdapter myAdapter;
+        private SqlConnection conn;
+        private string dbConnectionString = "Data Source=DESKTOP-R95RP8R;Initial Catalog=pingpong;Integrated Security=True";
+
+        /// <constructor>
+        /// Initialise Connection
+        /// </constructor>
+        public DbConnection()
+        {
+            myAdapter = new SqlDataAdapter();
+            conn = new SqlConnection(dbConnectionString);
+        }
+
+        /// <method>
+        /// Open Database Connection if Closed or Broken
+        /// </method>
+        private SqlConnection openConnection()
+        {
+            if (conn.State == ConnectionState.Closed || conn.State ==
+                        ConnectionState.Broken)
+            {
+                conn.Open();
+            }
+            return conn;
+        }
+
+        /// <method>
+        /// Close Database Connection if Open
+        /// </method>
+        private SqlConnection closeConnection()
+        {
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+            return conn;
+        }
+
+        /// <method>
+        /// Select Query
+        /// </method>
+        public DataTable executeSelectQuery(String _query, SqlParameter[] sqlParameter)
+        {
+            SqlCommand myCommand = new SqlCommand();
+            DataTable dataTable = new DataTable();
+            dataTable = null;
+            DataSet ds = new DataSet();
+            try
+            {
+                myCommand.Connection = openConnection();
+                myCommand.CommandText = _query;
+                myCommand.Parameters.AddRange(sqlParameter);
+                myCommand.ExecuteNonQuery();
+                myAdapter.SelectCommand = myCommand;
+                myAdapter.Fill(ds);
+                dataTable = ds.Tables[0];
+            }
+            catch (SqlException e)
+            {
+                Console.Write("Error - Connection.executeSelectQuery - Query: " 
+                    + _query + " \nException: " + e.StackTrace.ToString());
+                return null;
+            }
+            finally
+            {
+                myCommand.Connection = closeConnection();
+            }
+            return dataTable;
+        }
+
+        /// <method>
+        /// Insert Query
+        /// </method>
+        public bool executeInsertQuery(String _query, SqlParameter[] sqlParameter)
+        {
+            SqlCommand myCommand = new SqlCommand();
+            try
+            {
+                myCommand.Connection = openConnection();
+                myCommand.CommandText = _query;
+                myCommand.Parameters.AddRange(sqlParameter);
+                myAdapter.InsertCommand = myCommand;
+                myCommand.ExecuteNonQuery();
+            }
+            catch (SqlException e)
+            {
+                Console.Write("Error - Connection.executeInsertQuery - Query: " 
+                    + _query + " \nException: \n" + e.StackTrace.ToString());
+                return false;
+            }
+            finally
+            {
+                myCommand.Connection = closeConnection();
+            }
+            return true;
+        }
+
+        /// <method>
+        /// Update Query
+        /// </method>
+        public bool executeUpdateQuery(String _query, SqlParameter[] sqlParameter)
+        {
+            SqlCommand myCommand = new SqlCommand();
+            try
+            {
+                myCommand.Connection = openConnection();
+                myCommand.CommandText = _query;
+                myCommand.Parameters.AddRange(sqlParameter);
+                myAdapter.UpdateCommand = myCommand;
+                myCommand.ExecuteNonQuery();
+            }
+            catch (SqlException e)
+            {
+                Console.Write("Error - Connection.executeUpdateQuery - Query: "
+                    + _query + " \nException: " + e.StackTrace.ToString());
+                return false;
+            }
+            finally
+            {
+                myCommand.Connection = closeConnection();
+            }
+            return true;
+        }
+
+        /// <method>
+        /// Delete Query
+        /// </method>
+        public bool executeDeleteQuery(String _query, SqlParameter[] sqlParameter)
+        {
+            SqlCommand myCommand = new SqlCommand();
+            try
+            {
+                myCommand.Connection = openConnection();
+                myCommand.CommandText = _query;
+                myCommand.Parameters.AddRange(sqlParameter);
+                myAdapter.DeleteCommand = myCommand;
+                myCommand.ExecuteNonQuery();
+            }
+            catch (SqlException e)
+            {
+                Console.Write("Error - Connection.executeDeleteQuery - Query: "
+                    + _query + " \nException: " + e.StackTrace.ToString());
+                return false;
+            }
+            finally
+            {
+                myCommand.Connection = closeConnection();
+            }
+            return true;
+        }
+    }
+}
---
+++ SD1-pingpong/Dao/PlayerDAO.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Data;
+
+namespace Dao
+{
+    class PlayerDAO
+    {
+        private DbConnection conn;
+
+        /// <constructor>
+        /// Constructor UserDAO
+        /// </constructor>
+        public PlayerDAO()
+        {
+            conn = new DbConnection();
+        }
+
+        /// <method>
+        /// Get User Email By Firstname or Lastname and return DataTable
+        /// </method>
+        public DataTable searchByName(string _username)
+        {
+            string query = string.Format("select * from [t01_user] where t01_firstname like @t01_firstname or t01_lastname like @t01_lastname ");
+            SqlParameter[] sqlParameters = new SqlParameter[2];
+            sqlParameters[0] = new SqlParameter("@t01_firstname", SqlDbType.VarChar);
+            sqlParameters[0].Value = Convert.ToString(_username);
+            sqlParameters[1] = new SqlParameter("@t01_lastname", SqlDbType.VarChar);
+            sqlParameters[1].Value = Convert.ToString(_username);
+            return conn.executeSelectQuery(query, sqlParameters);
+        }
+
+        /// <method>
+        /// Get User Email By Id and return DataTable
+        /// </method>
+        public DataTable searchById(string _id)
+        {
+            string query = "select * from [t01_id] where t01_id = @t01_id";
+            SqlParameter[] sqlParameters = new SqlParameter[1];
+            sqlParameters[0] = new SqlParameter("@t01_id", SqlDbType.VarChar);
+            sqlParameters[0].Value = Convert.ToString(_id);
+            return conn.executeSelectQuery(query, sqlParameters);
+        }
+    }
+}
